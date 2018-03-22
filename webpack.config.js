f08b8c489aca2cac18d7c20220fb87b081447a31
Ilya Kozhevnikov <git@kozhevnikov.com>
2018-03-22 17:46:43 +0000

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
   mode: 'development',
@@ -15,26 +16,15 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({ use: 'css-loader' })
-      },
-      {
-        test: /\.vue$/,
-        use: {
-          loader: 'vue-loader',
-          options: { extractCSS: true }
-        }
-      }
+      { test: /\.vue$/, loader: 'vue-loader' },
+      { test: /\.html$/, loader: 'html-loader' },
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, use: ExtractTextPlugin.extract({ use: 'css-loader' }) }
     ]
   },
   plugins: [
     new CleanWebpackPlugin('dist'),
+    new VueLoaderPlugin(),
     new HtmlWebpackPlugin({ template: 'src/client/index.html' }),
     new ExtractTextPlugin('styles.css')
   ]
